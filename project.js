2c6a5130074f4f68fe82fc710138d8b2ff51be7e
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-13 17:13:47 +0100

    sudo code

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
-//1.
+// 1. Deposit money
+// 2. determine number of line to bet on
+// 3. collect bet amount
+// 4. spin slot machine
+// 5. check if user won
+// 6. give user their winnings
+// 7. play again
