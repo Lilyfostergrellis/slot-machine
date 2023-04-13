4543adc16dae6a8337c63482bc08234a87d1822c
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-04-13 17:38:07 +0100

    deposit money function in place

--- project.js
+++ project.js
@@ -5,3 +5,27 @@
 // 5. check if user won
 // 6. give user their winnings
 // 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+        //parseFloat to check if the user input is a number or NaN.
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, please try again.");
+        }
+        //if the deposit amount is not a number or the amount is less than or equal to 0 then the console log displays.
+        else {
+            return numberDepositAmount;
+        }
+        //loops infinitely until user enters valid amount
+    
+    }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
